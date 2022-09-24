1db45e9f5c74c61686c6b6a636797f25c9b459a5
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2022-09-24 14:10:36 +0200

    fix: remove redundant param

--- AttributeBasedRegistration/Attributes/InterceptedByAttribute.cs
+++ AttributeBasedRegistration/Attributes/InterceptedByAttribute.cs
@@ -13,12 +13,7 @@ public sealed class InterceptedByAttribute : Attribute
     /// Interceptor type.
     /// </summary>
     public Type Interceptor { get; private set; }
-    
-    /// <summary>
-    /// Interceptor lifetime.
-    /// </summary>
-    public ServiceLifetime InterceptorLifetime { get; private set; }
-    
+
     /// <summary>
     /// Registration order.
     /// </summary>
@@ -29,14 +24,12 @@ public sealed class InterceptedByAttribute : Attribute
     /// </summary>
     /// <param name="interceptor">Interceptor type.</param>
     /// <param name="registrationOrder">Registration order.</param>
-    /// <param name="interceptorLifetime">Interceptor's lifetime.</param>
-    public InterceptedByAttribute(int registrationOrder, Type interceptor, ServiceLifetime interceptorLifetime)
+    public InterceptedByAttribute(int registrationOrder, Type interceptor)
     {
         if (interceptor.GetInterfaces().All(x => x != typeof(IAsyncInterceptor) && x != typeof(IInterceptor)))
             throw new ArgumentException($"{interceptor.Name} does not implement any interceptor interface");
 
         Interceptor = interceptor;
         RegistrationOrder = registrationOrder;
-        InterceptorLifetime = interceptorLifetime;
     }
 }
