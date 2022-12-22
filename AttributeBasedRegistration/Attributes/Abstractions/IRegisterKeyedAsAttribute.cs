9a62e2a0f5df86d639a8a2d4b14907d065a2f794
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2022-12-22 19:15:09 +0100

    feat: split nugets to base and autofac, keyed registration support

---
+++ AttributeBasedRegistration/Attributes/Abstractions/IRegisterKeyedAsAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace AttributeBasedRegistration.Attributes.Abstractions;
+
+/// <summary>
+/// Defines as what should the keyed service be registered.
+/// </summary>
+public interface IRegisterKeyedAsAttribute : IRegistrationAttribute
+{
+    /// <summary>
+    /// Type to register given service as.
+    /// </summary>
+    Type ServiceType { get; }
+
+    /// <summary>
+    /// The key of the service.
+    /// </summary>
+    object Key { get; }
+}
---
+++ AttributeBasedRegistration/TypeHelper.cs
@@ -0,0 +1,32 @@
+﻿using AttributeBasedRegistration.Attributes.Abstractions;
+
+namespace AttributeBasedRegistration;
+
+internal static class TypeHelper
+{
+       internal static List<Type> GetServiceTypes(this Type type, IServiceImplementationAttribute implementationAttribute, IEnumerable<IRegisterAsAttribute> asAttributes)
+        => asAttributes.Where(x => x.ServiceTypes is not null)
+            .SelectMany(x => x.ServiceTypes ?? Type.EmptyTypes)
+            .Concat(implementationAttribute?.ServiceTypes ?? Type.EmptyTypes)
+            .Distinct()
+            .ToList();
+
+    internal static bool ShouldAsInterfaces(this Type type, IServiceImplementationAttribute implementationAttribute, IEnumerable<IRegisterAsAttribute> asAttributes)
+        => asAttributes.Any(x =>
+               x.RegistrationStrategy is RegistrationStrategy.AsImplementedInterfaces) ||
+           implementationAttribute?.RegistrationStrategy is RegistrationStrategy.AsImplementedInterfaces;
+    
+    internal static bool ShouldAsSelf(this Type type, IServiceImplementationAttribute implementationAttribute, IEnumerable<IRegisterAsAttribute> asAttributes, IEnumerable<Type> serviceTypes)
+        => (asAttributes.Any(x => x.RegistrationStrategy is RegistrationStrategy.AsSelf) ||
+            implementationAttribute?.RegistrationStrategy is RegistrationStrategy.AsSelf) &&
+           serviceTypes.All(y => y != type);
+    
+    internal static bool ShouldAsDirectAncestors(this Type type, IServiceImplementationAttribute implementationAttribute, IEnumerable<IRegisterAsAttribute> asAttributes)
+        => asAttributes.Any(x => x.RegistrationStrategy is RegistrationStrategy.AsDirectAncestorInterfaces) ||
+           implementationAttribute?.RegistrationStrategy is RegistrationStrategy.AsDirectAncestorInterfaces;
+    
+    internal static bool ShouldUsingNamingConvention(this Type type, IServiceImplementationAttribute implementationAttribute, IEnumerable<IRegisterAsAttribute> asAttributes)
+        => asAttributes.Any(x => x.RegistrationStrategy is RegistrationStrategy.AsConventionNamedInterface) ||
+           implementationAttribute?.RegistrationStrategy is RegistrationStrategy.AsConventionNamedInterface;
+
+}
