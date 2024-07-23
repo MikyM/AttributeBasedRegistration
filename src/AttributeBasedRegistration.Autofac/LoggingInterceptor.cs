acf14da92da1b8cfa1524cb057d6c8abd916dd3e
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2024-07-23 13:43:11 +0200

    feat: rework logging interceptor, dependencies

--- src/AttributeBasedRegistration.Autofac/LoggingInterceptor.cs
+++ src/AttributeBasedRegistration.Autofac/LoggingInterceptor.cs
@@ -25,49 +25,27 @@ public class LoggingInterceptor : AsyncInterceptorBase
         Func<IInvocation, IInvocationProceedInfo, Task> proceed)
     {
         var sw = new Stopwatch();
-        var args = new List<string>();
 
-        foreach (var arg in invocation.Arguments.Select((x, i) => new { i, x }))
-        {
-            string str = string.Empty;
-            dynamic obj = new ExpandoObject();
-            obj.Index = arg.i;
-            obj.ArgumentType = arg.x.GetType().Name;
-
-            try
-            {
-                str = JsonSerializer.Serialize(arg);
-            }
-            catch
-            {
-                obj.Value = "Couldnt serialize";
-                args.Add(JsonSerializer.Serialize(obj));
-            }
-
-            obj.Value = str;
-            args.Add(JsonSerializer.Serialize(obj));
-        }
-
-        args.Insert(0, "{[");
-        args.Add("]}");
-        var serializedArgs = string.Join(" ", args);
-
-        _logger.LogDebug($"Calling {invocation.Method.DeclaringType?.Name} {invocation.Method.Name} with args: {serializedArgs}");
+        _logger.LogTrace("Calling {DeclaringTypeName} {MethodName}", invocation.Method.DeclaringType?.Name, invocation.Method.Name);    
 
         try
         {
             sw.Start();
+            
             await proceed(invocation, proceedInfo).ConfigureAwait(false);
+            
             sw.Stop();
         }
         catch (Exception)
         {
             sw.Stop();
-            _logger.LogDebug($"Execution of {invocation.Method.DeclaringType?.Name} {invocation.Method.Name} with args: {serializedArgs} errored after {sw.Elapsed.TotalMilliseconds} ms");
+            
+            _logger.LogTrace("Execution of {DeclaringTypeName} {MethodName} errored after {ElapsedTotalMilliseconds} ms", invocation.Method.DeclaringType?.Name, invocation.Method.Name, sw.Elapsed.TotalMilliseconds); 
+            
             throw;
         }
 
-        _logger.LogDebug($"Finished executing {invocation.Method.DeclaringType?.Name} {invocation.Method.Name} with args: {serializedArgs} after {sw.Elapsed.TotalMilliseconds} ms");
+        _logger.LogTrace("Finished executing {DeclaringTypeName} {MethodName} after {ElapsedTotalMilliseconds} ms", invocation.Method.DeclaringType?.Name, invocation.Method.Name, sw.Elapsed.TotalMilliseconds);   
     }
 
     /// <inheritdoc />
@@ -75,51 +53,29 @@ public class LoggingInterceptor : AsyncInterceptorBase
         IInvocationProceedInfo proceedInfo, Func<IInvocation, IInvocationProceedInfo, Task<TResult>> proceed)
     {
         var sw = new Stopwatch();
-        var args = new List<string>();
 
-        foreach (var arg in invocation.Arguments.Select((x, i) => new { i, x }))
-        {
-            string str = string.Empty;
-            dynamic obj = new ExpandoObject();
-            obj.Index = arg.i;
-            obj.ArgumentType = arg.x.GetType().Name;
-
-            try
-            {
-                str = JsonSerializer.Serialize(arg);
-            }
-            catch
-            {
-                obj.Value = "Couldnt serialize";
-                args.Add(JsonSerializer.Serialize(obj));
-            }
-
-            obj.Value = str;
-            args.Add(JsonSerializer.Serialize(obj));
-        }
-
-        args.Insert(0, "{[");
-        args.Add("]}");
-        var serializedArgs = string.Join(" ", args);
-
-        _logger.LogDebug($"Calling {invocation.Method.DeclaringType?.Name} {invocation.Method.Name} with args: {serializedArgs}");
+        _logger.LogTrace("Calling {DeclaringTypeName} {MethodName}", invocation.Method.DeclaringType?.Name, invocation.Method.Name);        
 
         TResult? result;
 
         try
         {
             sw.Start();
+            
             result = await proceed(invocation, proceedInfo).ConfigureAwait(false);
+            
             sw.Stop();
         }
         catch (Exception)
         {
             sw.Stop();
-            _logger.LogDebug($"Execution of {invocation.Method.DeclaringType?.Name} {invocation.Method.Name} with args: {serializedArgs} errored after {sw.Elapsed.TotalMilliseconds} ms");
+
+            _logger.LogTrace("Execution of {DeclaringTypeName} {MethodName} errored after {ElapsedTotalMilliseconds} ms", invocation.Method.DeclaringType?.Name, invocation.Method.Name, sw.Elapsed.TotalMilliseconds); 
+            
             throw;
         }
 
-        _logger.LogDebug($"Finished executing {invocation.Method.DeclaringType?.Name} {invocation.Method.Name} with args: {serializedArgs} after {sw.Elapsed.TotalMilliseconds} ms");
+        _logger.LogTrace("Finished executing {DeclaringTypeName} {MethodName} after {ElapsedTotalMilliseconds} ms", invocation.Method.DeclaringType?.Name, invocation.Method.Name, sw.Elapsed.TotalMilliseconds);   
 
         return result;
     }
---
+++ src/AttributeBasedRegistration/Attributes/Abstractions/IImplementationOfAttribute.cs
@@ -0,0 +1,24 @@
+﻿namespace AttributeBasedRegistration.Attributes.Abstractions;
+
+/// <summary>
+/// Represents a service implementation attribute.
+/// </summary>
+/// <remarks> A newer equivalent of <see cref="IServiceImplementationAttribute"/>.</remarks>
+[PublicAPI]
+public interface IImplementationOfAttribute : IRegistrationAttribute
+{
+    /// <summary>
+    /// Lifetime to use for the registration if specified.
+    /// </summary>
+    ServiceLifetime? ServiceLifetime { get; }
+    
+    /// <summary>
+    /// Services types if specified.
+    /// </summary>
+    Type[]? ServiceTypes { get; }
+    
+    /// <summary>
+    /// Registration strategy if specified.
+    /// </summary>
+    RegistrationStrategy? RegistrationStrategy { get; }
+}
